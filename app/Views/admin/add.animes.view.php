a696eeff8769fb3df30b42b8b71a93f04029864c
DavidCounagoGonzalez <dcounagogonzalez@danielcastelao.org>
2024-05-19 14:10:00 +0200

    Terminado el form, pero el file-input está maldito

--- app/Views/admin/add.animes.view.php
+++ app/Views/admin/add.animes.view.php
@@ -14,7 +14,7 @@
                                 <h3 class="title">Añadir Anime</h3>
                             </div>
                             <div class="card-body">
-                                <form>
+                                <form method="post" enctype="multipart/form-data">
                                     <div class="row">
                                         <div class="col-md-5 pr-md-1">
                                             <div class="form-group">
@@ -22,13 +22,13 @@
                                                 <input type="text" class="form-control" placeholder="Título del Anime" value="">
                                             </div>
                                         </div>
-                                        <div class="col-md-3 px-md-1">
+                                        <div class="col-md-4 px-md-1">
                                             <div class="form-group">
                                                 <label>Episodios</label>
                                                 <input type="number" class="form-control" placeholder="Número de episodios" value="">
                                             </div>
                                         </div>
-                                        <div class="d-flex col-md-4 pl-md-20 align-items-center">
+                                        <div class="d-flex col-md-2 pl-md-20 align-items-center">
                                             <div class="form-check">
                                                 <label class="form-check-label">
                                                     <input class="form-check-input" type="checkbox" value="">
@@ -47,46 +47,64 @@
                                                 <input type="text" class="form-control" placeholder="Desde - Hasta">
                                             </div>
                                         </div>
-                                        <div class="col-md-6 pl-md-1">
+                                        <div class="col-md-3 pr-md-1">
                                             <div class="form-group">
-                                                <label>Last Name</label>
-                                                <input type="text" class="form-control" placeholder="Last Name" value="Andrew">
+                                                <label for="calificacion">Calificación</label>
+                                                <select class="form-control custom-select" id="calificacion">
+                                                    <option>Todos los públicos</option>
+                                                    <option>+12</option>
+                                                    <option>+16</option>
+                                                    <option>+18</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="form-group">
+                                                <label>Puntuación</label>
+                                                <input type="number" class="form-control" placeholder="Home Address" value="0.00" step=".01">
                                             </div>
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-md-12">
+                                        <div class="col-md-2 pr-md-1">
                                             <div class="form-group">
-                                                <label>Address</label>
-                                                <input type="text" class="form-control" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09">
+                                                <label for="selectDay">Día de la semana:</label>
+                                                <select class="form-control custom-select" id="selectDay" name="day">
+                                                    <option value="" selected disabled>Selecciona un día</option>
+                                                    <option value="lunes">Lunes</option>
+                                                    <option value="martes">Martes</option>
+                                                    <option value="miércoles">Miércoles</option>
+                                                    <option value="jueves">Jueves</option>
+                                                    <option value="viernes">Viernes</option>
+                                                    <option value="sábado">Sábado</option>
+                                                    <option value="domingo">Domingo</option>
+                                                </select>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-4 pr-md-1">
+                                        <div class="col-md-2 px-md-1">
                                             <div class="form-group">
-                                                <label>City</label>
-                                                <input type="text" class="form-control" placeholder="City" value="Mike">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4 px-md-1">
-                                            <div class="form-group">
-                                                <label>Country</label>
-                                                <input type="text" class="form-control" placeholder="Country" value="Andrew">
+                                                <label for="selectTime">Hora:</label>
+                                                <input type="time" class="form-control" id="selectTime" name="time">
                                             </div>
                                         </div>
                                         <div class="col-md-4 pl-md-1">
                                             <div class="form-group">
-                                                <label>Postal Code</label>
-                                                <input type="number" class="form-control" placeholder="ZIP Code">
+                                                <label for="subirImagen">Subir imagen:</label>
+                                                <input type="file" class="form-control" id="subirImagen" name="file" accept="image/*" multiple>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 pl-md-1">
+                                            <div class="form-group">
+                                                <label for="trailer">Enlace al trailer:</label>
+                                                <input type="text" class="form-control" id="trailer" name="trailer">
                                             </div>
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="col-md-8">
                                             <div class="form-group">
-                                                <label>About Me</label>
-                                                <textarea rows="4" cols="80" class="form-control" placeholder="Here can be your description" value="Mike">Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.</textarea>
+                                                <label>Sinopsis</label>
+                                                <textarea rows="4" cols="80" class="form-control" placeholder="Sinopsis del anime"></textarea>
                                             </div>
                                         </div>
                                     </div>
