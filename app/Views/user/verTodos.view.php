571173fa0e2b242102b759110507d2f86a52771c
DavidCounagoGonzalez <dcounagogonzalez@danielcastelao.org>
2024-06-08 21:59:09 +0200

    Filtros escondidos

--- app/Views/user/verTodos.view.php
+++ app/Views/user/verTodos.view.php
@@ -12,62 +12,69 @@
     <body>
         <div class="main-panel">
             <?php include 'templates/navbar.view.php' ?>
-            <div class="filters-container">
-                <h2>Filtros</h2>
-                <div class="form-group">
-                    <label for="genre-filter">Género:</label>
-                    <select id="genre-filter" class="form-control">
-                        <option value="all">Todos</option>
-                        <option value="accion">Acción</option>
-                        <option value="aventura">Aventura</option>
-                        <option value="comedia">Comedia</option>
-                        <!-- Agrega más opciones según tus géneros -->
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="status-filter">Estado de Emisión:</label>
-                    <select id="status-filter" class="form-control">
-                        <option value="all">Todos</option>
-                        <option value="en_emision">En Emisión</option>
-                        <option value="finalizado">Finalizado</option>
-                    </select>
-                </div>
-            </div>
-            <div class="row justify-content-center" id="cardContainer">
-                <?php foreach ($animes as $anime) { ?>
-                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mt-4 cardContainer">
-                        <div class="card mx-auto" style="background-image: url('<?php echo $anime['imagenes'] ?>');"
-                             data-descripcion="<?php echo $anime['descripcion'] ?>"
-                             data-episodios = " <?php echo $anime['episodios'] ?>"
-                             data-enEmision = " <?php echo $anime['en_emision'] ?> "
-                             data-emision = "<?php echo $anime['fecha_emision'] ?> "
-                             data-calificacion = "<?php echo $anime['calificacion'] ?>">
-                            <div class="card-body-overlay">
-                                <h5 class="card-title"><?php echo $anime['titulo'] ?></h5>
-                                <button class="btn btn-info guarda">Hola</button>
-                            </div>
+            <div class="content">
+                <div class="filters-container">
+                    <button class="btn btn-primary d-lg-none" type="button" data-toggle="collapse" data-target="#filtersCollapse" aria-expanded="false" aria-controls="filtersCollapse">
+                        Mostrar/Ocultar Filtros
+                    </button>
+                    <div class="collapse d-lg-block mt-2" id="filtersCollapse">
+                        <h2>Filtros</h2>
+                        <div class="form-group">
+                            <label for="genre-filter">Género:</label>
+                            <select id="genre-filter" class="form-control">
+                                <option value="all">Todos</option>
+                                <option value="accion">Acción</option>
+                                <option value="aventura">Aventura</option>
+                                <option value="comedia">Comedia</option>
+                                <!-- Agrega más opciones según tus géneros -->
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="status-filter">Estado de Emisión:</label>
+                            <select id="status-filter" class="form-control">
+                                <option value="all">Todos</option>
+                                <option value="en_emision">En Emisión</option>
+                                <option value="finalizado">Finalizado</option>
+                            </select>
                         </div>
                     </div>
-                <?php } ?>
+                </div>
+                <div class="row justify-content-center" id="cardContainer">
+                    <?php foreach ($animes as $anime) { ?>
+                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mt-4 cardContainer">
+                            <div class="card mx-auto" style="background-image: url('<?php echo $anime['imagenes'] ?>');"
+                                 data-descripcion="<?php echo $anime['descripcion'] ?>"
+                                 data-episodios = " <?php echo $anime['episodios'] ?>"
+                                 data-enEmision = " <?php echo $anime['en_emision'] ?> "
+                                 data-emision = "<?php echo $anime['fecha_emision'] ?> "
+                                 data-calificacion = "<?php echo $anime['calificacion'] ?>">
+                                <div class="card-body-overlay">
+                                    <h5 class="card-title"><?php echo $anime['titulo'] ?></h5>
+                                    <button class="btn btn-info guarda">Hola</button>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
 
+                </div>
+                <div class=" row pagination" id="pagination"></div>
             </div>
-            <div class=" row pagination" id="pagination"></div>
             <?php include 'templates/footer.view.php' ?>
-        </div>
-        <div class="modal fade" id="animeModal" tabindex="-1" aria-labelledby="animeModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="animeModalLabel"></h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p id="animeCalificacion"></p>
-                        <p id="animeEpisodios"></p>
-                        <p id="animeEmision"></p>
-                        <p id="animeDescripcion"></p>
+            <div class="modal fade" id="animeModal" tabindex="-1" aria-labelledby="animeModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="animeModalLabel"></h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p id="animeCalificacion"></p>
+                            <p id="animeEpisodios"></p>
+                            <p id="animeEmision"></p>
+                            <p id="animeDescripcion"></p>
+                        </div>
                     </div>
                 </div>
             </div>
